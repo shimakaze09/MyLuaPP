7568292bc628577242da9532185d1c17c1ff3e28
John <yangtianzhuo970425@gmail.com>
2025-03-12 15:37:11 +1300

    Enhance MyLuaPP with attribute support for Min functions and update project code style settings

--- src/test/01_overload/main.cpp
+++ src/test/01_overload/main.cpp
@@ -16,8 +16,14 @@ struct [[size(8)]] Point {
   float y;
   float Sum() { return x + y; }
   float Sum(float z) { return x + y + z; }
-  float Min() { return x < y ? x : y; }
-  float Min(float z) { return x < y ? (x < z ? x : z) : (y < z ? y : z); }
+  [[number(1024)]]
+  float Min() {
+    return x < y ? x : y;
+  }
+  [[number(520)]]
+  float Min(float z) {
+    return x < y ? (x < z ? x : z) : (y < z ? y : z);
+  }
 };
 
 template <>
@@ -32,8 +38,10 @@ struct TypeInfo<Point> : TypeInfoBase<Point> {
       Field{"y", &Point::y, AttrList{Attr{"info", "hello"}}},
       Field{"Sum", static_cast<float (Point::*)()>(&Point::Sum)},
       Field{"Sum", static_cast<float (Point::*)(float)>(&Point::Sum)},
-      Field{"Min", static_cast<float (Point::*)()>(&Point::Min)},
-      Field{"Min", static_cast<float (Point::*)(float)>(&Point::Min)}};
+      Field{"Min", static_cast<float (Point::*)()>(&Point::Min),
+            AttrList{Attr{"number", 1024}}},
+      Field{"Min", static_cast<float (Point::*)(float)>(&Point::Min),
+            AttrList{Attr{"number", 520}}}};
 
   static constexpr AttrList attrs = {Attr{"size", 8}};
 };
@@ -47,19 +55,21 @@ int main() {
   My::MyLuaPP::Register<Point>(L);
   sol::state_view lua(L);
   const char code[] = R"(
- p0 = Point.new(3, 4)                                       -- constructor
- p1 = Point.new()                                           -- constructor overload
- print(p0.x, p0.y)                                          -- get field
- p1.x = 3                                                   -- set field
- print(p1.x, p1.y)
- print(p0:Sum())                                            -- non-static member function
- print(MySRefl_TypeInfo.Point.attrs.size)                    -- MySRefl type attrs
- print(MySRefl_TypeInfo.Point.fields.x.attrs.not_serialize)  -- MySRefl field attrs
- print(MySRefl_TypeInfo.Point.fields.y.attrs.info)           -- MySRefl type attrs
- print(p0:Sum(2))                                           -- non-static member function overload
- print(p0:Min())                                            -- non-static member function overload
- print(p0:Min(-1))                                          -- non-static member function overload
- )";
+p0 = Point.new(3, 4)                                       -- constructor
+p1 = Point.new()                                           -- constructor overload
+print(p0.x, p0.y)                                          -- get field
+p1.x = 3                                                   -- set field
+print(p1.x, p1.y)
+print(p0:Sum())                                            -- non-static member function
+print(MySRefl_TypeInfo.Point.attrs.size)                    -- MySRefl type attrs
+print(MySRefl_TypeInfo.Point.fields.x.attrs.not_serialize)  -- MySRefl field attrs
+print(MySRefl_TypeInfo.Point.fields.y.attrs.info)           -- MySRefl type attrs
+print(p0:Sum(2))                                           -- non-static member function overload
+print(p0:Min())                                            -- non-static member function overload
+print(p0:Min(-1))                                          -- non-static member function overload
+print(MySRefl_TypeInfo.Point.fields.Min_0.attrs.number)     -- non-static member function overload
+print(MySRefl_TypeInfo.Point.fields.Min_1.attrs.number)     -- non-static member function overload
+)";
   cout << code << endl << "----------------------------" << endl;
   lua.script(code);
 
